70f47464920628dcf9acb63110f89e8d5e1f601a
“yogesh404emac” <yogesh404emac@gmail.com>
2023-10-16 22:19:36 +0530

    Recursive enumerations example

--- EnumsPlayground.playground/Contents.swift
+++ EnumsPlayground.playground/Contents.swift
@@ -75,3 +75,16 @@ case Tuesday
 func day() ->String { return self.rawValue }
 }
 print(WeekDay.Monday.day())
+
+
+//Recursive Enumerations
+indirect enum ArithmeticExpression {
+  case number(Int)
+  case addition(ArithmeticExpression, ArithmeticExpression)
+  case multiplication(ArithmeticExpression, ArithmeticExpression)
+}
+
+let two = ArithmeticExpression.number(2)
+let three = ArithmeticExpression.number(3)
+let sum = ArithmeticExpression.addition(two, three)
+print("Sum = \(sum)")
